cab69ca6848c506ab14408bd4b7bcfb79337eea5
msaoudallah <msa_oudallah@yahoo.com>
2016-06-20 05:59:19 +0200

    solved the following 1- added all images extensions 2- perform sort on images on the directory

--- imges2pdf.py
+++ imges2pdf.py
@@ -7,9 +7,9 @@ from PIL import Image
 import glob
 
 
-images_path = raw_input("Enter the path of the folder containing images : ")
-images =images_path+"/*.jpg"
 
+images_path = raw_input("Enter the path of the folder containing images : ")
+images =images_path+"/*.*"
 
 assert os.path.exists(images_path), "this diretory doesn't exist, "+str(images_path)
 f = os.chdir(images_path)
@@ -21,9 +21,20 @@ for filename in glob.glob(images):
     image_list.append(filename)
 
 pdf = FPDF( unit = 'mm')
-pos = 0.1
-for i in image_list:
-    
+
+imnames = [i.split("\\") for i in image_list] 
+imnames = [i[-1] for i in imnames ]
+imnums = [i.split('.') for i in imnames]
+imnums = [i[0] for i in imnums]
+imnums = [int(i) for i in imnums]
+
+
+
+pos = 0 
+images_dict = dict(zip(image_list, imnums))
+sorted_images = sorted(images_dict , key = images_dict.get)
+
+for i in list(sorted_images):
     pdf.add_page()
     im = Image.open(i)
     pdf.image(i,pos,pos,200,250)
